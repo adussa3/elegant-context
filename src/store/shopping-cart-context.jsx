985ef8fabee4f457d7ef090f84602c2a6339d4b3
Anunoy Dussa <adussa3@gmail.com>
2024-03-12 15:29:16 -0700

    Created a CartContextProvider component and used it in the App component

--- src/store/shopping-cart-context.jsx
+++ src/store/shopping-cart-context.jsx
@@ -1,4 +1,5 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
+import { DUMMY_PRODUCTS } from "../dummy-products.js";
 
 // You can pass a value in createContext that will be used as an initial value
 // that can be provided to multiple components
@@ -12,8 +13,76 @@ import { createContext } from "react";
 // const cartContext = useContext(CartContext) --> "cartContext.items" shows up in the auto completion
 //
 // While small, it makes your life as a developer easier!
-export const CartContext = createContext({
+const CartContext = createContext({
     items: [],
     addItemToCart: () => {},
     updateCartItemQuantity: () => {},
 });
+
+// We can create a component function in shopping-cart-context.jsx to get all the context-related data management
+// out of the App component (to make it less bloated) into a seperate context component
+export default function CartContextProvider({ children }) {
+    const [shoppingCart, setShoppingCart] = useState({
+        items: [],
+    });
+
+    function handleAddItemToCart(id) {
+        setShoppingCart((prevShoppingCart) => {
+            const updatedItems = [...prevShoppingCart.items];
+
+            const existingCartItemIndex = updatedItems.findIndex((cartItem) => cartItem.id === id);
+            const existingCartItem = updatedItems[existingCartItemIndex];
+
+            if (existingCartItem) {
+                const updatedItem = {
+                    ...existingCartItem,
+                    quantity: existingCartItem.quantity + 1,
+                };
+                updatedItems[existingCartItemIndex] = updatedItem;
+            } else {
+                const product = DUMMY_PRODUCTS.find((product) => product.id === id);
+                updatedItems.push({
+                    id: id,
+                    name: product.title,
+                    price: product.price,
+                    quantity: 1,
+                });
+            }
+
+            return {
+                items: updatedItems,
+            };
+        });
+    }
+
+    function handleUpdateCartItemQuantity(productId, amount) {
+        setShoppingCart((prevShoppingCart) => {
+            const updatedItems = [...prevShoppingCart.items];
+            const updatedItemIndex = updatedItems.findIndex((item) => item.id === productId);
+
+            const updatedItem = {
+                ...updatedItems[updatedItemIndex],
+            };
+
+            updatedItem.quantity += amount;
+
+            if (updatedItem.quantity <= 0) {
+                updatedItems.splice(updatedItemIndex, 1);
+            } else {
+                updatedItems[updatedItemIndex] = updatedItem;
+            }
+
+            return {
+                items: updatedItems,
+            };
+        });
+    }
+
+    const contextValue = {
+        items: shoppingCart.items,
+        addItemToCart: handleAddItemToCart,
+        updateCartItemQuantity: handleUpdateCartItemQuantity,
+    };
+
+    return <CartContext.Provider value={contextValue}>{children}</CartContext.Provider>;
+}
